2a009f3ddf1884ece14916a635e6f7ff63d754b1
bernas-ernesto <thonbernas1134@gmail.com>
2025-04-08 12:51:00 +0800

    Add initial file and distribution of tasks.

---
+++ main.py
@@ -0,0 +1,6 @@
+def main_menu(): #TODO BERNAS
+def list_record(): #TODO BUENACIFRA
+def add_student(): #TODO BERNAS
+def update_record(): #TODO BUENACIFRA
+def delete_record(): #TODO TERO
+def search_record(): #TODO ROLDAN
